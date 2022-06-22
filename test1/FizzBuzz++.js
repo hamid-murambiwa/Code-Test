5fe528d6e8c48199dc71d1c1b5547872b787c371
hamid-murambiwa <machipisajunior@gmail.com>
2022-06-22 14:48:29 +0200

    Bug fix

--- test1/FizzBuzz++.js
+++ test1/FizzBuzz++.js
@@ -20,8 +20,10 @@ function fizz() {
       console.log('FiZZBUZZ++');
     } else if (i % 3 === 0 && i % 5 === 0) {
       console.log('FIZZBUZZ');
+    } else if (i % 3 === 0) {
+      console.log('FIZZ');
     } else if (i % 5 === 0) {
-      console.log('Buzz');
+      console.log('BUZZ');
     } else {
       console.log(i);
     }
